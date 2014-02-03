8d26718711fcfcbff9bc962d7c4a3de9788daee6
Nathan LaFreniere <quitlahok@gmail.com>
2014-02-03 10:02:26 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+This util generates a crypt(3) compatible sha512 hash for things like apache, shadow files, and haproxy
+
+To use:
+
+```
+npm i -g mkpasswd
+mkpasswd
+```
---
+++ index.js
@@ -0,0 +1,17 @@
+var crypto = require('crypto');
+var sha512crypt = require('sha512crypt-node');
+var pw = require('pw');
+var salt = crypto.randomBytes(10).toString('base64');
+
+process.stdout.write('Password: ');
+pw(function (password) {
+    process.stdout.write('Password (confirm): ');
+    pw(function (password2) {
+        if (password !== password2) {
+            console.error('Passwords do not match');
+            process.exit(1);
+        }
+        console.log(sha512crypt.sha512crypt(password, salt));
+        process.exit(0);
+    });
+});
